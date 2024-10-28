2efa2f73fd9e2aeea222e73d16a5490ce789d322
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-10-28 13:47:07 +0100

    Add Ripple and RippleEffect components

---
+++ components/Ripple.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Ripple = () => {
+  return (
+    <View>
+      <Text>Ripple Effect</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Ripple;
---
+++ components/RippleEffect.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const RippleEffect = () => {
+  return (
+    <View>
+      <Text>Ripple Effect</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default RippleEffect;
