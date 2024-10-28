3514da227f5b172b293edc7d43bdfa58c3dd199c
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-10-28 14:04:35 +0100

    Refactor Ripple and RippleEffect components

--- components/Ripple.tsx
+++ components/Ripple.tsx
@@ -1,14 +1,102 @@
+import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Gesture, GestureDetector, GestureHandlerRootView } from 'react-native-gesture-handler';
+import Animated, {
+  measure,
+  runOnJS,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
-const Ripple = () => {
+interface IRipple {
+  style?: StyleProp<ViewStyle>;
+  onTap?: () => void;
+  children?: React.ReactNode;
+}
+
+const Ripple: React.FC<IRipple> = ({ style, onTap, children }) => {
+  const centerRippleShadowX = useSharedValue(0);
+  const centerRippleShadowY = useSharedValue(0);
+  const scale = useSharedValue(0);
+  const aRef = useAnimatedRef<View>();
+  const width = useSharedValue(0);
+  const height = useSharedValue(0);
+  const rippleOpacity = useSharedValue(0);
+
+  const tapGesture = Gesture.Tap()
+    .onBegin((event) => {
+      const layout = measure(aRef);
+      if (layout) {
+        width.value = layout.width;
+        height.value = layout.height;
+
+        centerRippleShadowX.value = event.x;
+        centerRippleShadowY.value = event.y;
+
+        rippleOpacity.value = 1;
+        scale.value = 0;
+        scale.value = withTiming(1, { duration: 600 });
+      } else {
+        console.warn('Layout is null');
+      }
+    })
+    .onTouchesDown((event) => {
+      if (onTap) {
+        runOnJS(onTap)();
+      }
+    })
+    .onEnd(() => {
+      rippleOpacity.value = withTiming(0);
+    })
+    .onTouchesCancelled(() => {
+      rippleOpacity.value = withTiming(0);
+    });
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    const circleRadiusOfShadowRipple = Math.sqrt(width.value ** 2 + height.value ** 2);
+
+    const x = centerRippleShadowX.value - circleRadiusOfShadowRipple;
+    const y = centerRippleShadowY.value - circleRadiusOfShadowRipple;
+
+    return {
+      width: circleRadiusOfShadowRipple * 2,
+      height: circleRadiusOfShadowRipple * 2,
+      borderRadius: circleRadiusOfShadowRipple,
+      backgroundColor: 'rgba(255, 99, 71, 0.3)',
+      position: 'absolute',
+      opacity: rippleOpacity.value,
+      top: 0,
+      left: 0,
+
+      transform: [
+        {
+          translateX: x,
+        },
+        {
+          translateY: y,
+        },
+        { scale: scale.value },
+      ],
+    };
+  });
+
+  const gesture = Gesture.Exclusive(tapGesture);
   return (
-    <View>
-      <Text>Ripple Effect</Text>
+    <View ref={aRef} collapsable={false} style={style}>
+      <GestureHandlerRootView>
+        <GestureDetector gesture={gesture}>
+          <Animated.View style={[style, { overflow: 'hidden' }]}>
+            <View style={style}>{children}</View>
+            <Animated.View style={[reanimatedStyle]} />
+          </Animated.View>
+        </GestureDetector>
+      </GestureHandlerRootView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
-
 export default Ripple;
+
+const styles = StyleSheet.create({});
--- components/RippleEffect.tsx
+++ components/RippleEffect.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Ripple from './Ripple';
 
 const RippleEffect = () => {
+  const tapHandler = () => {};
+
   return (
-    <View>
-      <Text>Ripple Effect</Text>
+    <View style={styles.container}>
+      <Ripple style={styles.ripple} onTap={tapHandler}>
+        <Text style={{ fontSize: 20, color: '#ff6347', fontWeight: 'bold' }}>Press Me</Text>
+      </Ripple>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
-
 export default RippleEffect;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  ripple: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    backgroundColor: '#ffe0ce',
+
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    // Shadow iOS
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.33,
+    shadowRadius: 2.22,
+
+    // Shadow Android
+    elevation: 5,
+  },
+});
